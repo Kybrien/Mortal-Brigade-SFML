611746aff8a535073e67ce86c1f838c1da8ea419
Euexia <115560425+Euexia@users.noreply.github.com>
2023-12-15 03:28:04 +0100

    Force

---
+++ Level1.h
@@ -0,0 +1,143 @@
+#pragma once
+#include "RectangleShapeRenderer.h"
+#include "Scene.h"
+#include "TileMap.h"
+#include "Player.h"
+#include "SquareCollider.h"
+#include "SpriteRenderer.h"
+#include "Inventory.h"
+#include "Light.h"
+#include "ProximityPrompt.h"
+#include "Collectable.h"
+
+class Level1 final : public Scene
+{
+public:
+	Level1() : Scene("Level1")
+	{
+		GameObject* map = CreateMapGameObject("Map", "map_1");
+
+		GameObject* door = CreateProximityPromptGameObject("Door1", Maths::Vector2f(32 * 25.f, 32 * 26.f), 20.f, "Test");
+
+		GameObject* enemy = CreateREDMonsterGameObject("Enemy", Maths::Vector2f(32 * 25.f, 32 * 25.f));
+
+		GameObject* mine = CreateMineGameObject("Mine", Maths::Vector2f(32 * 26.f, 32 * 26.f));
+
+		GameObject* player = CreatePlayerGameObject("Player", Maths::Vector2f(32 * 25.f, 32 * 25.f));
+
+		GameObject* coral = CreateCollectableGameObject("Coral", Maths::Vector2f(32 * 25.f, 32 * 25.f), "Coral.png", 25.f, "Coral", 10.f);
+		GameObject* ore = CreateCollectableGameObject("Ore", Maths::Vector2f(32 * 26.f, 32 * 26.f), "Ore.png", 25.f, "Ore", 20.f);
+		GameObject* amethyst = CreateCollectableGameObject("Amethyst", Maths::Vector2f(32 * 27.f, 32 * 26.f), "Amethyst.png", 25.f, "Amethyst", 30.f);
+		GameObject* azurite = CreateCollectableGameObject("Azurite", Maths::Vector2f(32 * 28.f, 32 * 26.f), "Azurite.png", 25.f, "Azurite", 40.f);
+
+
+		//GameObject* enemy2 = CreateDummyGameObject("Enemy2", 0.f, sf::Color::Green);
+
+	}
+
+	GameObject* CreateMapGameObject(const std::string& _name, const std::string& map)
+	{
+		GameObject* game_object = CreateGameObject(_name);
+
+		TileMap* tilemap = game_object->CreateComponent<TileMap>();
+		tilemap->LoadMap(map, *this);
+
+		return game_object;
+	}
+
+	GameObject* CreatePlayerGameObject(const std::string& _name, const Maths::Vector2f _position)
+	{
+		GameObject* game_object = CreateGameObject(_name);
+		game_object->SetPosition(_position);
+
+		Player* player_component = game_object->CreateComponent<Player>();
+		player_component->SetCurrentScene(this);
+
+		SpriteRenderer* sprite_renderer = game_object->CreateComponent<SpriteRenderer>();
+		sprite_renderer->LoadSprite("Walk.png");
+		sprite_renderer->SetTextureSize(Maths::Vector2u(360, 300));
+		sprite_renderer->SetScale(0.15f);
+		sprite_renderer->SetAnimSpeed(0.5f);
+		sprite_renderer->SetOffset(Maths::Vector2i(70, 70));
+
+		SquareCollider* square_collider = game_object->CreateComponent<SquareCollider>();
+		square_collider->SetWidth(32.f);
+		square_collider->SetHeight(32.f);
+
+		Inventory* inventory = game_object->CreateComponent<Inventory>();
+
+		Light* flashlight = game_object->CreateComponent<Light>();
+		flashlight->SetShapes(GetLightColliders());
+
+		return game_object;
+	}
+
+	GameObject* CreateREDMonsterGameObject(const std::string& _name, const Maths::Vector2f _position)
+	{
+		GameObject* game_object = CreateGameObject(_name);
+		game_object->SetPosition(_position);
+
+		SpriteRenderer* sprite_renderer = game_object->CreateComponent<SpriteRenderer>();
+		sprite_renderer->LoadSprite("RED.png");
+		sprite_renderer->SetTextureSize(Maths::Vector2u(48, 48));
+		sprite_renderer->SetScale(0.8f);
+		sprite_renderer->SetAnimSpeed(0.5f);
+		sprite_renderer->SetAutoIncrement(true);
+		sprite_renderer->SetBegin(sf::Vector2i(0, 1));
+		sprite_renderer->SetEnd(sf::Vector2i(9, 1));
+		sprite_renderer->SetOffset(Maths::Vector2i(5, 5));
+
+		return game_object;
+	}
+
+	GameObject* CreateMineGameObject(const std::string& _name, const Maths::Vector2f _position)
+	{
+		GameObject* game_object = CreateGameObject(_name);
+		game_object->SetPosition(_position);
+
+		SpriteRenderer* sprite_renderer = game_object->CreateComponent<SpriteRenderer>();
+		sprite_renderer->LoadSprite("Mine.png");
+		sprite_renderer->SetTextureSize(Maths::Vector2u(360, 360));
+		sprite_renderer->SetScale(0.05f);
+		sprite_renderer->SetOffset(Maths::Vector2i(0, 0));
+
+		return game_object;
+	}
+
+	GameObject* CreateProximityPromptGameObject(const std::string& _name, const Maths::Vector2f _position, const float _max_activation_distance, const std::string _text)
+	{
+		GameObject* game_object = CreateGameObject(_name);
+		game_object->SetPosition(_position);
+
+		ProximityPrompt* proximity_prompt = game_object->CreateComponent<ProximityPrompt>();
+		proximity_prompt->SetCurrentScene(this);
+		proximity_prompt->SetMaxActivationDistance(_max_activation_distance);
+		proximity_prompt->SetActionText(_text);
+
+		return game_object;
+	}
+
+	GameObject* CreateCollectableGameObject(const std::string& _name, const Maths::Vector2f _position, std::string _texture, const float _max_activation_distance, const std::string _text, float _price)
+	{
+		GameObject* game_object = CreateGameObject(_name);
+		game_object->SetPosition(_position);
+
+		SpriteRenderer* sprite_renderer = game_object->CreateComponent<SpriteRenderer>();
+		sprite_renderer->LoadSprite(_texture);
+		sprite_renderer->SetTextureSize(Maths::Vector2u(32, 32));
+		sprite_renderer->SetScale(1.f);
+		sprite_renderer->SetAnimSpeed(0.5f);
+		//sprite_renderer->SetAutoIncrement(true);
+		//sprite_renderer->SetBegin(sf::Vector2i(0, 0));
+		//sprite_renderer->SetEnd(sf::Vector2i(0, 0));
+		sprite_renderer->SetOffset(Maths::Vector2i(0, 0));
+
+		Collectable* collectable = game_object->CreateComponent<Collectable>();
+		collectable->SetCurrentScene(this);
+		collectable->SetMaxActivationDistance(_max_activation_distance);
+		collectable->SetActionText(_text);
+		collectable->SetPrice(_price);
+
+		return game_object;
+	}
+};
