c25fb4f9502848077b9d9a640590a3fb2411ccb8
KakaTheReal <kais.difallah@gmail.com>
2023-12-20 20:07:29 +0100

    without zip

--- SFMLEngine/include/Modules/SceneModule.h
+++ SFMLEngine/include/Modules/SceneModule.h
@@ -33,14 +33,14 @@ private:
 template<typename T>
 Scene* SceneModule::SetScene(const bool _replace_scenes)
 {
-	//if (_replace_scenes)
-	//{
-	//	for (const Scene* scene : scenes)
-	//	{
-	//		delete scene;
-	//	}
-	//	
-	//}
+	/*if (_replace_scenes)
+	{
+		for (const Scene* scene : scenes)
+		{
+			delete scene;
+		}
+		
+	}*/
 	scenes.clear();
 	Scene* scene = static_cast<Scene*>(new T());
 	scenes.push_back(scene);
